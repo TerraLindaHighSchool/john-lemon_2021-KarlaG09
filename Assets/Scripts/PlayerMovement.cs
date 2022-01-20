0d06a5add58707e0de91da67b723e9358c4eafcd
KarlaG09 <kg02793@srcs.org>
2022-01-20 15:40:52 -0800

    The environment 1-5

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -24,12 +24,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        // Get user input
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        // Use user input to set direction player will move
+        moveDirection.Set(horizontal, 0f, vertical);
+        moveDirection.Normalize();
+        // Set animator to walking or idle depending on user input
+        IsWalking = !(Mathf.Approximately(horizontal, 0f) && Mathf.Approximately(vertical, 0f));
+        animator.SetBool("IsWalking", IsWalking);
+        // Assign rotation towards move direction
+        Vector3 desiredDirection = Vector3.RotateTowards(transform.forward, moveDirection, turnSpeed * Time.deltaTime, 0f);
+        rotation = Quaternion.LookRotation(desiredDirection);
     }
-
     // Animator event
     private void OnAnimatorMove()
     {
-
+        rb.MovePosition(rb.position + moveDirection * animator.deltaPosition.magnitude);
+        rb.MoveRotation(rotation);
     }
 }
